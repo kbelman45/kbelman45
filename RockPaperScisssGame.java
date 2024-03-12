0f917814f1c9299743764ff93b1751273194a4e4
kbelman45 <159741867+kbelman45@users.noreply.github.com>
2024-03-12 02:56:28 -0500

    Update and rename RockPaperScissorsGame (1).java to RockPaperScisssGame.java

--- RockPaperScisssGame.java
+++ RockPaperScisssGame.java
@@ -3,25 +3,24 @@ import java.util.Scanner;
 import java.util.Random;
 
 /*
- * Glaycon Cezarotto
- * Object Programming 
- * 2-26-24
- * The purpose is to play a rock paper scissor game with the user.
+ * Kevin Belman
+ * Object Programming class
+ * 2/26/2024
+ * The purpose is to play a rock paper scissor game with the user of the programm.
  */
 public class RockPaperScissorsGame {
 	
 	//Displays the intro banner
 	private static void printWelcomeBanner() {
-        System.out.println("Welcome to the Rock, Paper, Scissors Game!");
-        System.out.println("The rules are:");
+        System.out.println("Welcome to the Rock, Paper, Scissors Game! Lets enjoy this!!");
+        System.out.println("The rules are of this game:");
         System.out.println("Use 1 for Rock, 2 for Paper, 3 for Scissors");
     }
-	//Randomizes the comuter's answer 
+	//Randomizes the comuter's answer in this programm
 	private static int generateComputerChoice() {
         Random random = new Random();
         return random.nextInt(3) + 1; 
     }
-	//Asks for the player's choice and catches errors of wrong numbers or letter inputs
 	private static int getUserChoice(Scanner scanner) {
         System.out.println("Enter your choice (1 for Rock, 2 for Paper, 3 for Scissors):");
         while (true) {
@@ -37,29 +36,29 @@ public class RockPaperScissorsGame {
             }
         }
     }
-	//Displays the Computer's random choice 
+	//Displays 
 	 private static void displayComputerChoice(int computerChoice) {
 	        switch (computerChoice) {
-	            case 1 -> System.out.println("Computer chose Rock");
-	            case 2 -> System.out.println("Computer chose Paper");
-	            case 3 -> System.out.println("Computer chose Scissors");
+	            case 1 -> System.out.println("Computer chooses Rock");
+	            case 2 -> System.out.println("Computer choses Paper");
+	            case 3 -> System.out.println("Computer choses Scissors");
 	        }
 	    }
-	 //Determines who won the game
+	 //Determines who won the game for ths programm
 	 private static void determineWinner(int computerChoice, int userChoice) {
 	        if (computerChoice == userChoice) {
-	            System.out.println("It's a tie! Let's play again.");
+	            System.out.println("It's a great tie! WOWWWW LETS PLAY AGAIN?.");
 	        } else if ((computerChoice == 1 && userChoice == 3) ||
 	                   (computerChoice == 2 && userChoice == 1) ||
 	                   (computerChoice == 3 && userChoice == 2)) {
-	            System.out.println("Computer wins!");
+	            System.out.println("Computer wins the game!!!!");
 	        } else {
-	            System.out.println("You win!");
+	            System.out.println("You win the gameee great game player!");
 	        }
 	    }
-	 // the loop to ask the player if they want to restart
+	 // the loop to ask the player if they want to restart the game
 	 private static boolean PlayAgain(Scanner scanner) {
-	        System.out.println("Do you want to play again? (Y/N):");
+	        System.out.println("Do you want to play again??? (Y/N):");
 	        String input = scanner.next();
 	     // Loop until the user inputs a valid response (y or n) 
 	        while (!input.equalsIgnoreCase("Y") && !input.equalsIgnoreCase("N")) {
@@ -82,7 +81,7 @@ public class RockPaperScissorsGame {
 	            keepPlaying = PlayAgain(scanner);
 	        }
 
-	        System.out.println("Thank you for playing!");
+	        System.out.println("Thank you for playing, see you again!");
 	        scanner.close();
 
 	}
